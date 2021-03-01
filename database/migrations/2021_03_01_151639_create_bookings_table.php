6dfae4c1b33ea96ce3fe8ef39022fb6b64dfa984
Samuel Ricko <srickopp@gmail.com>
2021-03-01 22:25:53 +0700

    🌱[INIT] Models, migrations, seeders

---
+++ database/migrations/2021_03_01_151639_create_bookings_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateBookingsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('bookings', function (Blueprint $table) {
+            $table->id('booking_id');
+            $table->timestamp('created_at_local');
+            $table->integer('driver_id')->unsigned();
+            $table->integer('passenger_id');
+            $table->string('state');
+            $table->integer('country_id');
+            $table->float('fare', 11, 2);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('bookings');
+    }
+}
