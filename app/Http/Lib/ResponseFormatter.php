f48c16e5cc9afa3839e3fe5aaa4addf2c98238e4
Samuel Ricko <srickopp@gmail.com>
2021-03-01 23:34:32 +0700

    ➕[ADD] Response formatter

---
+++ app/Http/Lib/ResponseFormatter.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace App\Lib;
+/**
+ * This class is used to format JSON response for the API
+ */
+class ResponseFormatter {
+
+    protected static $response = [
+        'meta' => [
+            'code' => 200,
+            'status' => 'success',
+            'message' => null
+        ],
+        'data' => null
+    ];
+
+    /** Define handler for response formatter for a success transactions */
+    public static function success($data = null, $message = null){
+        // Using self because the variable is a static typed.
+        self::$response['meta']['message'] = $message;
+        self::$response['data'] = $data;
+
+        return response()->json(self::$response, self::$response['meta']['code']);
+    }
+
+    /** Define handler to return response error */
+    public static function error($data = null, $message = null, $code = 400)
+    {
+        self::$response['meta']['status'] = 'error';
+        self::$response['meta']['code'] = $code;
+        self::$response['meta']['message'] = $message;
+        self::$response['data'] = $data;
+
+        return response()->json(self::$response, self::$response['meta']['code']);
+    }
+}
